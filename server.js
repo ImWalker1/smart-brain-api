9f6c922ee44ae8dc2e59fb1a88606cdf6c01a9fd
ImWalker <myfirstfakeid2013@yahoo.com>
2021-11-05 13:40:34 +0200

    files for the server

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controlers/register')
+const signin = require('./controlers/signin')
+const profile = require('./controlers/profile')
+const image = require('./controlers/image')
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    //port : 3306,
+    user : 'postgres',
+    password : 'Bogdan 53',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/', (req, res)=>{
+	res.send(database.users)
+})
+app.post('/signin', (req, resp)=>{signin.handleSignin(req, resp, db, bcrypt)} )
+app.post('/register', (req, res)=>{register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (reg, resp)=>{ profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res)=>{ image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res)=>{ image.handleApiCall(req, res)})
+
+ app.listen (3000, ()=>{
+ 	console.log('App is runnning on porst 3000')
+ })
+
+
